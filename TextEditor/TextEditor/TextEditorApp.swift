f83269ca9b9baa176ebc4965a7029b46ff666d48
dedrasher <78565796+dedrasher@users.noreply.github.com>
2021-07-26 15:25:45 +0300

    Add files via upload

---
+++ TextEditor/TextEditor/TextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditorApp.swift
+//  TextEditor
+//
+//  Created by Serega on 09.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

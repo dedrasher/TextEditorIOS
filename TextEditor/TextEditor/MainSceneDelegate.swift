c6e3b35e41c40237ad42b9f0a889ecbf8f0dc1fc
dedrasher <78565796+dedrasher@users.noreply.github.com>
2021-08-24 15:16:55 +0300

    Add files via upload

---
+++ TextEditor/TextEditor/MainSceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  MainSceneDelegate.swift
+//  MainSceneDelegate
+//
+//  Created by Serega on 23.08.2021.
+//
+import SwiftUI
+import UIKit
+
+final class MainSceneDelegate: UIResponder, UIWindowSceneDelegate {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if connectionOptions.shortcutItem != nil{
+            Preferences.openNewFileEditingView = true
+        }
+    }
+    func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        Preferences.openNewFileEditingView = true
+    }
+}
